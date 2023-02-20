4dc0ea7ab2e9d32dd8ba70c66b5f4508b40b96b9
Ase's Son <bahaeggtaifi17@gmail.com>
2023-02-20 16:11:24 +0100

    Add

---
+++ Knighttron/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Knighttron
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Log = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.Log);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(720, 480);
+            this.panel1.TabIndex = 1;
+            // 
+            // Log
+            // 
+            this.Log.AutoSize = true;
+            this.Log.BackColor = System.Drawing.Color.Black;
+            this.Log.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Log.ForeColor = System.Drawing.Color.White;
+            this.Log.Location = new System.Drawing.Point(12, 9);
+            this.Log.Name = "Log";
+            this.Log.Size = new System.Drawing.Size(100, 42);
+            this.Log.TabIndex = 5;
+            this.Log.Text = "deez\r\n";
+            this.Log.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Log.Click += new System.EventHandler(this.Log_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(649, 421);
+            this.Controls.Add(this.panel1);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Knighttron App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label Log;
+    }
+}
+
---
+++ Knighttron/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Knighttron.DiscordRPC;
+using System.Security.Cryptography;
+using System.Threading;
+
+namespace Knighttron
+{
+    public partial class Form1 : Form
+    {
+        public float scale = 1f;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) { }
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+        public static Process p;
+
+        private DiscordRpc.EventHandlers handlers;
+        private DiscordRpc.RichPresence presence;
+
+        void DiscordRPC()
+        {
+            string appid = "1077210514708516864";
+            this.handlers = default(DiscordRpc.EventHandlers);
+            DiscordRpc.Initialize(appid, ref this.handlers, true, null);
+            this.handlers = default(DiscordRpc.EventHandlers);
+            DiscordRpc.Initialize(appid, ref this.handlers, true, null);
+            setuprpc();
+            this.presence.startTimestamp = DateTimeOffset.Now.ToUnixTimeSeconds();
+            DiscordRpc.UpdatePresence(ref this.presence);
+        }
+
+        void setuprpc()
+        {
+            this.presence.details = "Playing Knighttron v1.2.1";
+            this.presence.largeImageKey = "icon";
+        }
+
+        void dfi()
+        {
+            foreach (var flashprocess in Process.GetProcessesByName("rgf"))
+            {
+                try
+                {
+                    flashprocess.Kill();
+                }
+                catch (Exception ex)
+                {
+                    ShowPopup("Something is preventing Knighttron from making necessary actions, try restarting your pc. \n Detailed Error Info:\n" + ex, "Knighttron - Error", MessageBoxIcon.Error);
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                dfi();
+                DiscordRPC();
+                VerifyFile("rgf.exe", "0e9e4c81b7037b50acef88b9285d983bc096faf02bea5165187ba5c056cbcce0");
+                VerifyFile("discord-rpc-w32.dll", "517240600b04d454cc5ab7b03e43c4af5a0b831fd2515f25c015a83652ad4cac");
+                this.MaximizeBox = false;
+                this.FormBorderStyle = FormBorderStyle.FixedSingle;
+                Log.Text = "";
+                Log.Text += "Starting game...";
+                Log.Visible = true;
+                this.Cursor = Cursors.Hand;
+                string serverurl = "https://cache.armorgames.com/files/games/knighttron-17722.swf?v=1443468804";
+                string gd = serverurl + "";
+                string pd = "rgf.exe";
+                Log.Visible = false;
+                ProcessStartInfo si = new ProcessStartInfo(pd);
+                si.WindowStyle = ProcessWindowStyle.Minimized;
+                si.Arguments = gd;
+                p = Process.Start(si);
+                p.WaitForInputIdle();
+                SetParent(p.MainWindowHandle, panel1.Handle);
+                MoveWindow(p.MainWindowHandle, -8, -50, 665, 480, true);
+                p.ToString();
+            }
+            catch (Exception chkex)
+            {
+                ShowPopup("Something is preventing Knighttron from making necessary actions, try restarting your pc. If the issue persists, contact me on discord: dyzqy#8466", "Knighttron - Error", MessageBoxIcon.Error);
+                dfi();
+                Environment.Exit(0);
+            }
+            while (true)
+            {
+                if (p.HasExited)
+                {
+                    Environment.Exit(0);
+                }
+                try
+                {
+                    MoveWindow(p.MainWindowHandle, -8, -50, 665, 480, true);
+                }
+                catch (Exception me)
+                {
+                    dfi();
+                    Environment.Exit(0);
+                }
+                await Wait(1);
+            }
+        }
+
+        public static void ShowPopup(string message, string title, MessageBoxIcon icon)
+        {
+            MessageBox.Show(
+                message,
+                title,
+                MessageBoxButtons.OK,
+                icon
+            );
+        }
+
+        public async Task Wait(int milsec)
+        {
+            await Task.Delay(milsec);
+            this.Enabled = true;
+            this.Cursor = Cursors.Default;
+        }
+
+        private void Log_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void VerifyFile(string path, string hash)
+        {
+            if (File.Exists(path))
+            {
+                if (ComputeHash(path) != hash)
+                {
+                    ShowPopup("Vaffanculo skid11!!!!1!!111!!!!1!11!!!\nYour parents don't love you btw! :)", "Knighttron - No Skids Allowed!!!!", MessageBoxIcon.Error);
+                    dfi();
+                    Environment.Exit(0);
+                }
+            }
+            else
+            {
+                ShowPopup($"Failed to locate \"{path}\", please ensure it's next to this exe!", "Knighttron - Error", MessageBoxIcon.Error);
+                dfi();
+                Environment.Exit(0);
+            }
+        }
+
+        string ComputeHash(string path)
+        {
+            return BitConverter.ToString(SHA256.Create().ComputeHash(File.OpenRead(path))).Replace("-", "").ToLowerInvariant();
+        }
+
+        void SpawnSkidMsg()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
